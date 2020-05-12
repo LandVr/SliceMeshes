9985069a892fae2633050bc99136291cbbb5a456
LandVr <65224392+LandVr@users.noreply.github.com>
2020-05-12 12:41:57 +0200

    Slicer Files

---
+++ SliceListener.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SliceListener : MonoBehaviour
+{
+    public Slicer slicer;
+    private void OnTriggerEnter(Collider other)
+    {
+        slicer.isTouched = true;
+    }
+}
